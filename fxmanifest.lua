721904846d9c38137b952f6cfcfe9251cbccd311
SamBradley123 <69010639+SamBradley123@users.noreply.github.com>
2024-01-13 18:38:01 +0100

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'Podz'
+description 'SpawnConvoi'
+version '1.0.0'
+
+client_scripts {
+    'clientevents.lua',
+   
+}
+
+server_script {
+  'servevents.lua'
+  }
+
+
+shared_script '@es_extended/imports.lua'
+
+  
